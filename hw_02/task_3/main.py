3cf5c10c4e90e64f62af34d9c3edef6c27f7a684
nanashi <egorglazkov71@gmail.com>
2025-03-05 16:54:34 +0300

    add backend for task 3 in hw 2

---
+++ hw_02/task_3/main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+
+from pydantic import BaseModel
+import logging
+
+from parcels_model import ParcelsDB, ParcelsModel
+
+
+logger = logging.getLogger("uvicorn.error")
+app = FastAPI()
+parcels_db = ParcelsDB()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/parcels")
+def get_parcels(status: str, destination: str):
+    parcels = parcels_db.get_parcels()
+    if status is not None:
+        parcels = list(filter(lambda parcel: parcel.status == status, parcels))
+    if destination is not None:
+        parcels = list(filter(lambda parcel: parcel.destination == destination, parcels))
+
+    logger.info(f"GET/parcels: {parcels}")
+    return {"status": "ok", "parcels": parcels}
+
+
+@app.post("/parcels")
+def add_parcels(model: ParcelsModel):
+    parcels_db.add_parcel(model)
+
+    logger.info(f"POST/add-parcels: {model.name}")
+    return {"status": "ok"}
+
+
+@app.patch("/parcels/{id}")
+def update_parcels_status(id: int, new_status: str):
+    parcels_db.update_status(id=id, status=new_status)
+
+    logger.info(f"PATCH/update-status: {id} - {new_status}")
+    return {"status": "ok"}
+
+
+@app.delete("/parcels/{id}")
+def delete_parcels(id: int):
+    parcels_db.remove_parcel(id)
+
+    logger.info(f"DELETE/parcels: {id}")
+    return {"status": "ok"}
+
+
+app.mount("/", StaticFiles(directory="./static", html=True), name="static")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
---
+++ hw_02/task_3/parcels_model.py
@@ -0,0 +1,72 @@
+import sqlite3
+
+from pydantic import BaseModel
+
+
+class ParcelsModel(BaseModel):
+    id: int | None = None
+    name: str
+    status: str = "in-transit"
+    destination: str
+    recipient: str
+    weight: float
+
+
+class ParcelsDB:
+    def __init__(self):
+        self.__connection = sqlite3.Connection("parcels_.db")
+        self.__cursor = self.__connection.cursor()
+        self.create_db()
+    
+    def create_db(self):
+        self.__cursor.execute("""
+        CREATE TABLE IF NOT EXISTS parcel (
+        id          INT PRIMARY KEY,
+        name            TEXT NOT NULL,
+        status          TEXT NOT NULL,
+        destination     TEXT NOT NULL,
+        recipient       TEXT NOT NULL,
+        weight          FLOAT NOT NULL
+        )
+        """)
+    
+    def get_parcels(self):
+        self.__cursor.execute("""SELECT * FROM parcel""")
+
+        parcels_tuple = self.__cursor.fetchall()
+        parcels = []
+
+        for (id, name, status, destination, recipient, weight) in parcels_tuple:
+            parcel = ParcelsModel(id=id, name=name, status=status, destination=destination, recipient=recipient, weight=weight)
+            parcels.append(parcel)
+
+        return parcels
+
+    def add_parcel(self, model: ParcelsModel):
+        self.__cursor.execute("""
+        INSERT INTO parcel(id, name, status, destination, recipient, weight)
+        VALUES(?, ?, ?, ?, ?, ?)
+        """, (model.id, model.name, model.status, model.destination, model.recipient, model.weight))
+
+        self.__connection.commit()
+
+    def remove_parcel(self, id: int):
+        self.__cursor.execute("""
+        DELETE FROM parcel WHERE id = ?
+        """, (id, ))
+
+        self.__connection.commit()
+
+    def update_status(self, id: int, status: str):
+        self.__cursor.execute("""
+        UPDATE parcel SET status = ? WHERE id = ?
+        """, (status, id))
+
+        self.__connection.commit()
+
+    def __get_last_id(self):
+        parcels = self.get_parcels()
+
+        return min(i.id for i in parcels) + 1
+
+    def 
